ea62ed34ee865d7bbbc3e4952601a80f0e8965aa
Bennette321321 <6631321321@student.chula.ac.th>
2026-04-27 16:53:26 +0700

    feat: add referendum ocr

--- utils.py
+++ utils.py
@@ -5,17 +5,20 @@ import json
 OCR_OUTPUT_DIR = "ocr_output"
 
 # เก็บผลลัพธ์การ ocr
-def save_ocr_output(file_name: str, page_num: int, data_json: dict, data_csv: pd.DataFrame):
+def save_ocr_output(file_name: str, page_num: int, summary: dict, df: pd.DataFrame):
     base_name = os.path.basename(file_name)
     dir_name = os.path.dirname(file_name).replace('data/', '').replace(".pdf", "")
     
     os.makedirs(OCR_OUTPUT_DIR, exist_ok=True)
     os.makedirs(f"{OCR_OUTPUT_DIR}/{dir_name}", exist_ok=True)
     
+    result = {
+        "summary": summary,
+        "data": df.to_dict(orient="records") if df is not None else []
+    }
+    
     with open(f"{OCR_OUTPUT_DIR}/{dir_name}/{base_name}_{page_num}.json", "w", encoding="utf-8") as f:
-        json.dump(data_json, f, ensure_ascii=False, indent=2)
-        
-    data_csv.to_csv(f"{OCR_OUTPUT_DIR}/{dir_name}/{base_name}_{page_num}.csv", index=False)
+        json.dump(result, f, ensure_ascii=False, indent=2)
 
 # แปลงเลขไทยเป็นเลขอารบิก
 def thai_to_arabic(text):
@@ -40,3 +43,20 @@ def swap_if_party_is_numeric(row):
     if party_val.isdigit():
         return pd.Series({'พรรค': row['คะแนน'], 'คะแนน': party_val})
     return row
+
+# ดึงข้อมูล file
+def load_data(file_name: str, page_num: int):
+    key = f"{file_name}_{page_num}"
+    
+    with open(f'ocr_output/{file_name}_{page_num}.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    summary = data.get("summary", {})
+    df = pd.DataFrame(data.get("data", []))
+    
+    return {
+        key: {
+            "df": df,
+            "summary": summary
+        }
+    }
